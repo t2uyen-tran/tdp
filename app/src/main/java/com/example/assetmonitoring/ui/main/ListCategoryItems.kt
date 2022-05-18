fc4e528a019cac0efdfb3a318405df71e43f49de
Tracy <103538753@student.swin.edu.au>
2022-05-18 18:20:43 +1000

    revised version
    revised version

--- app/src/main/java/com/example/assetmonitoring/ui/main/ListCategoryItems.kt
+++ app/src/main/java/com/example/assetmonitoring/ui/main/ListCategoryItems.kt
@@ -20,6 +20,12 @@ import com.example.assetmonitoring.R
 import com.example.assetmonitoring.model.Categories
 import com.example.assetmonitoring.ui.council.OutstandingActivity
 import com.facebook.appevents.suggestedevents.ViewOnClickListener
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -33,13 +39,17 @@ import java.util.*
 
 private const val TAG = "ListCategoryItems"
 
-class ListCategoryItems : AppCompatActivity(), View.OnClickListener {
+class ListCategoryItems : AppCompatActivity(), OnMapReadyCallback {
 
-
+    private lateinit var mMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onStart()called")
         setContentView(R.layout.list_category_items)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         val categoryNameTV = findViewById<TextView>(R.id.categoryListTitle_textV)
 
@@ -51,12 +61,9 @@ class ListCategoryItems : AppCompatActivity(), View.OnClickListener {
 
         categoryNameTV.text = categoryName?.let { getString(it) }
 
-        val listView = findViewById<ListView>(R.id.footpathitems_listV)
+        val listView = findViewById<AutoCompleteTextView>(R.id.footpathitems_listV)
         val nextPage = findViewById<Button>(R.id.next_button)
 
-        // camera and upload btn
-        val uploadBtn = findViewById<Button>(R.id.uploadBtn)
-        val cameraBtn = findViewById<ImageButton>(R.id.camera)
 
         //create the list of items for selection
         //https://www.youtube.com/watch?v=rxRuW2qZ2ZY
@@ -64,13 +71,13 @@ class ListCategoryItems : AppCompatActivity(), View.OnClickListener {
 
         val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(
             this,
-            android.R.layout.simple_list_item_single_choice,
+            android.R.layout.simple_dropdown_item_1line,
             categoryItems
         )
 
-        listView.adapter = arrayAdapter
+        listView.setAdapter(arrayAdapter)
 
-        listView.choiceMode = ListView.CHOICE_MODE_SINGLE
+       // listView.choiceMode = ListView.CHOICE_MODE_SINGLE
 
         var selectedItemName = ""
 
@@ -101,207 +108,22 @@ class ListCategoryItems : AppCompatActivity(), View.OnClickListener {
         }
 
 
-        //upload photo button and take photo button
-        uploadBtn.setOnClickListener(this)
-        cameraBtn.setOnClickListener(this)
 
     }
 
-    override fun onClick(v: View?) {
-        when (v!!.id) {
-            R.id.uploadBtn -> {
-                val pictureDialog = AlertDialog.Builder(this)
-                pictureDialog.setTitle("Select Action")
-                val pictureDialogItems =
-                    arrayOf("Select photo from gallery")
-                pictureDialog.setItems(
-                    pictureDialogItems
-                ) { _, which ->
-                    when (which) {
-                        // Here we have create the methods for image selection from GALLERY
-                        0 -> choosePhotoFromGallery()
-                    }
-                }
-                pictureDialog.show()
-            }
-            R.id.camera -> {
-                val pictureDialog = AlertDialog.Builder(this)
-                pictureDialog.setTitle("Select Action")
-                val pictureDialogItems =
-                    arrayOf("Capture photo from camera")
-                pictureDialog.setItems(
-                    pictureDialogItems
-                ) { _, which ->
-                    when (which) {
-                        // Here we have create the methods for image selection from GALLERY
-                        0 -> takePhotoFromCamera()
-                    }
-                }
-                pictureDialog.show()
-            }
-        }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
 
-    }
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val ivPlaceImage  = findViewById<ImageView>(R.id.iv_place_image)
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == GALLERY) {
-                if (data != null) {
-                    val contentURI = data.data
-                    try {
-                        // Here this is used to get an bitmap from URI
-                        @Suppress("DEPRECATION")
-                        val selectedImageBitmap =
-                            MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
-                        val saveImageToInternalStorage =
-                            saveImageToInternalStorage(selectedImageBitmap)
-                        ivPlaceImage.setImageBitmap(selectedImageBitmap)
-                        Log.e("Saved Image : ", "Path :: $saveImageToInternalStorage")
-
-                        // Set the selected image from GALLERY to imageView.
-                        Toast.makeText(this@ListCategoryItems, "Successfully added from upload", Toast.LENGTH_SHORT)
-                            .show()
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                        Toast.makeText(this@ListCategoryItems, "Failed!", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                }
-            }else if (requestCode == CAMERA) {
-
-                val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap // Bitmap from camera
-                val saveImageToInternalStorage =
-                    saveImageToInternalStorage(thumbnail)
-                ivPlaceImage.setImageBitmap(thumbnail)
-                Log.e("Saved Image : ", "Path :: $saveImageToInternalStorage")
-                Toast.makeText(this@ListCategoryItems, "Successfully added from camera", Toast.LENGTH_SHORT)
-                    .show() // Set to the imageView.
-            }
-
-        }
-    }
-
-    private fun takePhotoFromCamera() {
-        Dexter.withActivity(this)
-            .withPermissions(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA
-            )
-            .withListener(object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    // Here after all the permission are granted launch the CAMERA to capture an image.
-                    if (report!!.areAllPermissionsGranted()) {
-                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                        startActivityForResult(intent, CAMERA)
-                    }
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    permissions: MutableList<PermissionRequest>?,
-                    token: PermissionToken?
-                ) {
-                    showRationalDialogForPermissions()
-                }
-            }).onSameThread()
-            .check()
-        Toast.makeText(this, "camera", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun choosePhotoFromGallery() {
-        Dexter.withContext(this)
-            .withPermissions(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            )
-            .withListener(object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-
-                    // Here after all the permission are granted launch the gallery to select and image.
-                    if (report!!.areAllPermissionsGranted()) {
-                        val galleryIntent = Intent(
-                            Intent.ACTION_PICK,
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                        )
-
-                        startActivityForResult(galleryIntent, GALLERY)
-                        Toast.makeText(this@ListCategoryItems, "camera", Toast.LENGTH_SHORT).show()
-
-                    }
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    permissions: MutableList<PermissionRequest>?,
-                    token: PermissionToken?
-                ) {
-                    showRationalDialogForPermissions()
-                }
-            }).onSameThread()
-            .check()
-    }
-    private fun showRationalDialogForPermissions() {
-        AlertDialog.Builder(this)
-            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
-            .setPositiveButton(
-                "GO TO SETTINGS"
-            ) { _, _ ->
-                try {
-                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri = Uri.fromParts("package", packageName, null)
-                    intent.data = uri
-                    startActivity(intent)
-                } catch (e: ActivityNotFoundException) {
-                    e.printStackTrace()
-                }
-            }
-            .setNegativeButton("Cancel") { dialog,
-                                           _ ->
-                dialog.dismiss()
-            }.show()
-    }
-    private fun saveImageToInternalStorage(bitmap: Bitmap): Uri {
-
-        // Get the context wrapper instance
-        val wrapper = ContextWrapper(applicationContext)
-
-        // Initializing a new file
-        // The bellow line return a directory in internal storage
-        /**
-         * The Mode Private here is
-         * File creation mode: the default mode, where the created file can only
-         * be accessed by the calling application (or all applications sharing the
-         * same user ID).
-         */
-        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
-
-        // Create a file to save the image
-        file = File(file, "${UUID.randomUUID()}.jpg")
-
-        try {
-            // Get the file output stream
-            val stream: OutputStream = FileOutputStream(file)
-
-            // Compress bitmap
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-
-            // Flush the stream
-            stream.flush()
-
-            // Close stream
-            stream.close()
-        } catch (e: IOException) { // Catch the exception
-            e.printStackTrace()
-        }
-
-        // Return the saved image uri
-        return Uri.parse(file.absolutePath)
-    }
-    companion object {
-        private const val GALLERY = 1
-        private const val CAMERA = 2
-        private const val IMAGE_DIRECTORY = "AssetImages"
-        // A constant variable for place picker
-        private const val PLACE_AUTOCOMPLETE_REQUEST_CODE = 3
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-37.783, 144.95)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL)
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(16F))
+        mMap.getUiSettings().setZoomControlsEnabled(true)
+        mMap.getUiSettings().setCompassEnabled(true)
+        mMap.getUiSettings().setZoomGesturesEnabled(true)
+        mMap.getUiSettings().setScrollGesturesEnabled(true)
+        mMap.getUiSettings().setRotateGesturesEnabled(true)
     }
 }
--- app/src/main/java/com/example/assetmonitoring/ui/main/ReportIssueActivity.kt
+++ app/src/main/java/com/example/assetmonitoring/ui/main/ReportIssueActivity.kt
@@ -1,8 +1,21 @@
 package com.example.assetmonitoring.ui.main
 
+import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import com.example.assetmonitoring.R
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,9 +23,19 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
 
-class ReportIssueActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var mMap: GoogleMap
+class ReportIssueActivity : AppCompatActivity(),View.OnClickListener{
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,27 +67,212 @@ class ReportIssueActivity : AppCompatActivity(), OnMapReadyCallback {
         backButton.setOnClickListener {
             onBackPressed()
         }
+        // camera and upload btn
+        val uploadBtn = findViewById<Button>(R.id.uploadBtn)
+        val cameraBtn = findViewById<ImageButton>(R.id.camera)
+        //upload photo button and take photo button
+        uploadBtn.setOnClickListener(this)
+        cameraBtn.setOnClickListener(this)
 
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+    }
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.uploadBtn -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems =
+                    arrayOf("Select photo from gallery")
+                pictureDialog.setItems(
+                    pictureDialogItems
+                ) { _, which ->
+                    when (which) {
+                        // Here we have create the methods for image selection from GALLERY
+                        0 -> choosePhotoFromGallery()
+                    }
+                }
+                pictureDialog.show()
+            }
+            R.id.camera -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems =
+                    arrayOf("Capture photo from camera")
+                pictureDialog.setItems(
+                    pictureDialogItems
+                ) { _, which ->
+                    when (which) {
+                        // Here we have create the methods for image selection from GALLERY
+                        0 -> takePhotoFromCamera()
+                    }
+                }
+                pictureDialog.show()
+            }
+        }
+
+    }
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val ivPlaceImage  = findViewById<ImageView>(R.id.iv_place_image)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                if (data != null) {
+                    val contentURI = data.data
+                    try {
+                        // Here this is used to get an bitmap from URI
+                        @Suppress("DEPRECATION")
+                        val selectedImageBitmap =
+                            MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                        val saveImageToInternalStorage =
+                            saveImageToInternalStorage(selectedImageBitmap)
+                        ivPlaceImage.setImageBitmap(selectedImageBitmap)
+                        Log.e("Saved Image : ", "Path :: $saveImageToInternalStorage")
+
+                        // Set the selected image from GALLERY to imageView.
+                        Toast.makeText(this@ReportIssueActivity, "Successfully added from upload", Toast.LENGTH_SHORT)
+                            .show()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(this@ReportIssueActivity, "Failed!", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }else if (requestCode == CAMERA) {
+
+                val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap // Bitmap from camera
+                val saveImageToInternalStorage =
+                    saveImageToInternalStorage(thumbnail)
+                ivPlaceImage.setImageBitmap(thumbnail)
+                Log.e("Saved Image : ", "Path :: $saveImageToInternalStorage")
+                Toast.makeText(this@ReportIssueActivity, "Successfully added from camera", Toast.LENGTH_SHORT)
+                    .show() // Set to the imageView.
+            }
+
+        }
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    private fun takePhotoFromCamera() {
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    // Here after all the permission are granted launch the CAMERA to capture an image.
+                    if (report!!.areAllPermissionsGranted()) {
+                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        startActivityForResult(intent, CAMERA)
+                    }
+                }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-37.783, 144.95)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL)
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(16F))
-        mMap.getUiSettings().setZoomControlsEnabled(true)
-        mMap.getUiSettings().setCompassEnabled(true)
-        mMap.getUiSettings().setZoomGesturesEnabled(true)
-        mMap.getUiSettings().setScrollGesturesEnabled(true)
-        mMap.getUiSettings().setRotateGesturesEnabled(true)
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+        Toast.makeText(this, "camera", Toast.LENGTH_SHORT).show()
     }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                    // Here after all the permission are granted launch the gallery to select and image.
+                    if (report!!.areAllPermissionsGranted()) {
+                        val galleryIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+
+                        startActivityForResult(galleryIntent, GALLERY)
+                        Toast.makeText(this@ReportIssueActivity, "camera", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+    }
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog,
+                                           _ ->
+                dialog.dismiss()
+            }.show()
+    }
+    private fun saveImageToInternalStorage(bitmap: Bitmap): Uri {
+
+        // Get the context wrapper instance
+        val wrapper = ContextWrapper(applicationContext)
+
+        // Initializing a new file
+        // The bellow line return a directory in internal storage
+        /**
+         * The Mode Private here is
+         * File creation mode: the default mode, where the created file can only
+         * be accessed by the calling application (or all applications sharing the
+         * same user ID).
+         */
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+
+        // Create a file to save the image
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            // Get the file output stream
+            val stream: OutputStream = FileOutputStream(file)
+
+            // Compress bitmap
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+
+            // Flush the stream
+            stream.flush()
+
+            // Close stream
+            stream.close()
+        } catch (e: IOException) { // Catch the exception
+            e.printStackTrace()
+        }
+
+        // Return the saved image uri
+        return Uri.parse(file.absolutePath)
+    }
+    companion object {
+        private const val GALLERY = 1
+        private const val CAMERA = 2
+        private const val IMAGE_DIRECTORY = "AssetImages"
+        // A constant variable for place picker
+        private const val PLACE_AUTOCOMPLETE_REQUEST_CODE = 3
+    }
+
 
 }
 
