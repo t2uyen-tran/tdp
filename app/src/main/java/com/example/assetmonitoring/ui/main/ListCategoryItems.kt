89ddd2c67d9b241acbd6b4a7e442af6a89d08b7a
Tracy <103538753@student.swin.edu.au>
2022-05-18 23:58:26 +1000

    GPS
    GPS

--- app/src/main/java/com/example/assetmonitoring/ui/main/ListCategoryItems.kt
+++ app/src/main/java/com/example/assetmonitoring/ui/main/ListCategoryItems.kt
@@ -1,14 +1,19 @@
 package com.example.assetmonitoring.ui.main
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
+import android.location.Location
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
@@ -20,12 +25,17 @@ import com.example.assetmonitoring.R
 import com.example.assetmonitoring.model.Categories
 import com.example.assetmonitoring.ui.council.OutstandingActivity
 import com.facebook.appevents.suggestedevents.ViewOnClickListener
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -36,20 +46,26 @@ import java.io.FileOutputStream
 import java.io.IOException
 import java.io.OutputStream
 import java.util.*
+import kotlin.properties.Delegates
 
 private const val TAG = "ListCategoryItems"
 
-class ListCategoryItems : AppCompatActivity(), OnMapReadyCallback {
-
+class ListCategoryItems : AppCompatActivity(), OnMapReadyCallback, View.OnClickListener {
+    private lateinit var mapET: EditText
+    private lateinit var curTV: TextView
     private lateinit var mMap: GoogleMap
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient // A fused location client variable which is further user to get the user's current location
+    private var mLatitude: Double = 0.0 // A variable which will hold the latitude value.
+    private var mLongitude: Double = 0.0 // A variable which will hold the longitude value.
+    private var m1Latitude:  Double = 0.0 // A variable which will hold the latitude value.
+    private var m1Longitude: Double = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onStart()called")
         setContentView(R.layout.list_category_items)
 
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        // Initialize the Fused location variable
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         val categoryNameTV = findViewById<TextView>(R.id.categoryListTitle_textV)
 
@@ -107,17 +123,108 @@ class ListCategoryItems : AppCompatActivity(), OnMapReadyCallback {
             onBackPressed()
         }
 
+        if (!Places.isInitialized()) {
+            Places.initialize(
+                this@ListCategoryItems,
+                resources.getString(R.string.google_maps_api_key)
+            )
+        }
+
+        mapET = findViewById(R.id.addressInput_ET)
+        mapET.setOnClickListener(this)
+        curTV = findViewById(R.id.tv_select_current_location)
+        curTV.setOnClickListener(this)
+    }
 
 
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.addressInput_ET -> {
+                try {
+                    // These are the list of fields which we required is passed
+                    val fields = listOf(
+                        Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG,
+                        Place.Field.ADDRESS
+                    )
+                    // Start the autocomplete intent with a unique request code.
+                    val intent =
+                        Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+                            .build(this@ListCategoryItems)
+                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+//            R.id.tv_select_current_location -> {
+//                if (!isLocationEnabled()) {
+//                    Toast.makeText(
+//                        this,
+//                        "Your location provider is turned off. Please turn it on.",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//
+//                    // This will redirect you to settings from where you need to turn on the location provider.
+//                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+//                    startActivity(intent)
+//                } else {
+//                    // For Getting current location of user please have a look at below link for better understanding
+//                    // https://www.androdocs.com/kotlin/getting-current-location-latitude-longitude-in-android-using-kotlin.html
+//                    Dexter.withActivity(this)
+//                        .withPermissions(
+//                            Manifest.permission.ACCESS_FINE_LOCATION,
+//                            Manifest.permission.ACCESS_COARSE_LOCATION
+//                        )
+//                        .withListener(object : MultiplePermissionsListener {
+//                            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+//                                if (report!!.areAllPermissionsGranted()) {
+//
+//                                   requestNewLocationData()
+//                                }
+//                            }
+//
+//                            override fun onPermissionRationaleShouldBeShown(
+//                                permissions: MutableList<PermissionRequest>?,
+//                                token: PermissionToken?
+//                            ) {
+//                                Toast.makeText(
+//                                    this@ListCategoryItems,
+//                                    "2",
+//                                    Toast.LENGTH_SHORT
+//                                ).show()
+//                            }
+//                        }).onSameThread()
+//                        .check()
+//                }
+//            }
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+
+                val place: Place = Autocomplete.getPlaceFromIntent(data!!)
+
+                mapET.setText(place.address)
+                m1Latitude = place.latLng!!.latitude
+                Log.e("Current Latitude111", "$m1Latitude")
+
+                m1Longitude = place.latLng!!.longitude
+            Log.e("Current Latitude222", "$m1Longitude")
+            val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-37.783, 144.95)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val location = LatLng(m1Latitude, m1Longitude)
+        mMap.addMarker(MarkerOptions().position(location))
+        mMap.moveCamera((CameraUpdateFactory.newLatLng(location)))
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL)
         mMap.moveCamera(CameraUpdateFactory.zoomTo(16F))
         mMap.getUiSettings().setZoomControlsEnabled(true)
@@ -125,5 +232,55 @@ class ListCategoryItems : AppCompatActivity(), OnMapReadyCallback {
         mMap.getUiSettings().setZoomGesturesEnabled(true)
         mMap.getUiSettings().setScrollGesturesEnabled(true)
         mMap.getUiSettings().setRotateGesturesEnabled(true)
+    }
+
+    /**
+     * A function which is used to verify that the location or let's GPS is enable or not of the user's device.
+     */
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    /**
+     * A function to request the current location. Using the fused location provider client.
+     */
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    /**
+     * A location callback object of fused location provider client where we will get the current location details.
+     */
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            mLatitude = mLastLocation.latitude
+            Log.e("Current Latitude", "$mLatitude")
+            mLongitude = mLastLocation.longitude
+            Log.e("Current Longitude", "$mLongitude")
+
+
+        }
+    }
+
+    companion object {
+        // A constant variable for place picker
+        private const val PLACE_AUTOCOMPLETE_REQUEST_CODE = 3
     }
 }
