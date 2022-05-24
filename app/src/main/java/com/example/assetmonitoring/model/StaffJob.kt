d56cda7c52e624c797403bb924c90573eb70f182
CiXwOw <cixwow@gmail.com>
2022-05-24 17:52:40 +1000

    Specified keys in comments

--- app/src/main/java/com/example/assetmonitoring/model/StaffJob.kt
+++ app/src/main/java/com/example/assetmonitoring/model/StaffJob.kt
@@ -5,7 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class StaffJob (
-    val jobID: String,          // The ID of this job
+    val jobID: String,          // The ID of this job **THIS IS THE KEY**
     val staffID: String,        // The ID of the staff assigned to this job
     val caseID: String,         // The ID of the case for this job
     val estimatedTime: String,  // The estimated time to complete the task
