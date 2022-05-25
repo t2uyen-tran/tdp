cd91cc604871598d48a9acd442b79ff23df3d777
CiXwOw <cixwow@gmail.com>
2022-05-25 21:05:49 +1000

    Change date to Long in CaseContributor

--- app/src/main/java/com/example/assetmonitoring/model/CaseContributor.kt
+++ app/src/main/java/com/example/assetmonitoring/model/CaseContributor.kt
@@ -7,7 +7,7 @@ import java.sql.Timestamp
 @Parcelize
 data class CaseContributor (
     val userID: String,     // The ID of the user making the contribution
-    val date: Timestamp,    // The date the contribution was made
+    val date: Long,    // The date the contribution was made
     val notify: Boolean,    // Whether or not the user wishes to be notified of updates
     val comment: String,    // Text comment/description of issue
     val photo: String       // Link to image location
